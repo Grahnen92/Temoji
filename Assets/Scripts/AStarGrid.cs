feb3cdf97a325054268a50bf66ef626a2a9c7294
Hedlundaren <hedlundaren93@hotmail.com>
2016-09-23 08:21:07 +0800

    double array added if navmaps would not work

---
+++ Assets/Scripts/AStarGrid.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AStarGrid : MonoBehaviour {
+
+    private int gridSize = 10;
+    int[,] grid = new int[10, 10];
+	// Use this for initialization
+	void Start () {
+	    
+        for(int i = 0; i < gridSize; i++)
+        {
+            for (int j = 0; j < gridSize; j++)
+            {
+                // Initialize gridcell
+                print(grid[i,j]);
+
+
+            }
+        }
+
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
