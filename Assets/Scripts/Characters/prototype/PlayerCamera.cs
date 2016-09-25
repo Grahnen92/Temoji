ba0574ff22fb59ab0147111e1a22eba96de9a176
olle <olle>
2016-09-25 08:31:23 +0800

    Added prototype camera and did some minor cleanup

---
+++ Assets/Scripts/Characters/prototype/PlayerCamera.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerCamera : MonoBehaviour {
+	public GameObject player;
+	private Vector3 offset;
+	private Vector3 diff_vec;
+	private float distance;
+	private Rigidbody rb;
+
+	void Start () {
+		rb = GetComponent<Rigidbody> ();
+		offset = transform.position - player.transform.position;
+	}
+
+	void LateUpdate () {
+		//transform.position = player.transform.position + offset;
+		Vector3 diff_vec = (player.transform.position + offset) - transform.position;
+		diff_vec.y = 0.0f;
+		distance = diff_vec.magnitude;
+		diff_vec.Normalize();
+		//rb.AddForce (distance);
+		rb.velocity = diff_vec*distance*distance;
+	}
+}
---
+++ Assets/Scripts/Characters/prototype/player_weapon_controller.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class player_weapon_controller : MonoBehaviour 
+{
+	private GameObject owner;
+	private Vector3 offset;
+	private Rigidbody rb;
+	private Vector3 wanted_pos;
+	private Vector3 current_pos;
+	// Use this for initialization
+	void Start () 
+	{
+		owner = GameObject.Find("Player");
+		rb = GetComponent<Rigidbody> ();
+		offset = transform.position - owner.transform.position;
+		current_pos = new Vector3 (0.0f, 0.0f, 1.0f);
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () 
+	{
+		Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+		RaycastHit hit;
+		Physics.Raycast (ray, out hit, 100);
+		Vector3 tmp_vec = hit.point - owner.transform.position;
+		tmp_vec.y = 0;
+		//tmp_vec.Normalize();
+		Vector3 weapon_pos = owner.transform.position + tmp_vec;
+
+		//Planar movement
+		//float swingH = Input.GetAxis ("HorizontalRight");
+		//float swingV = Input.GetAxis ("VerticalRight");
+		//Vector3 weapon_pos = new Vector3 (swingH, 0, swingV);
+
+
+		//Vector3 weapon_pos = new Vector3 (1.0f, 0, 1.0f);
+		if (weapon_pos.magnitude > 0.001f) 
+		{
+			current_pos = weapon_pos;
+		}
+		//transform.position = (owner.transform.position + current_pos);
+		transform.position = weapon_pos;
+
+	}
+}
