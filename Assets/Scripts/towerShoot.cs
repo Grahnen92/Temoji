b560eef0ceb281322a2c02e72edd7aa82cfe0b9b
Skyer Liu <lkysmlfr@gmail.com>
2016-09-27 10:52:15 +0800

    In branch tower_shoot, added codes.docx and issues.txt in folder Temoji, added towerShoot.cs and towerShootInput.cs in folder Scripts, added bulletPrefab.prefab and towerPrefab.prefab

---
+++ Assets/Scripts/towerShoot.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class towerShoot : MonoBehaviour {
+    public float speed = 5.0f;
+    public GameObject player;
+	// Use this for initialization
+	void Start () {
+        print("Tower shooting begins");
+        Rigidbody rb = GetComponent<Rigidbody>();
+        Vector3 direction = player.transform.TransformVector(-1, 0, 0);
+        rb.AddForce(direction * speed * 100);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (transform.position.y<-5) {
+            Destroy(this.gameObject);
+        }
+	}
+}
---
+++ Assets/Scripts/towerShootInput.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class towerShootInput : MonoBehaviour
+{
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+    public Transform bullet;
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Instantiate(bullet, transform.position, transform.localRotation);
+        }
+    }
+}
