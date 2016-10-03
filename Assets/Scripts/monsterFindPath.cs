7d847b7e5228756f901c9910429b93030546548c
Y L <liying_211@163.com>
2016-10-03 14:24:11 +0800

    add navmesh test object

---
+++ Assets/Scripts/monsterFindPath.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class monsterFindPath : MonoBehaviour {
+
+    public NavMeshAgent monster;
+    public Transform target;
+	// Use this for initialization
+	void Start () {
+        monster = GetComponent<NavMeshAgent>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        monster.SetDestination(target.position);
+	}
+}
