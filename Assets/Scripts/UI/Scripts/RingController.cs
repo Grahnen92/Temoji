7bf6c27bb54f58a30134605ef0d0758a5a2f09dc
MythicWrath <jinxiang91@Hotmail.com>
2016-10-20 19:19:36 +0800

    Added Shooting animation

---
+++ Assets/Scripts/UI/Scripts/RingController.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RingController : MonoBehaviour {
+
+    public GameObject bullet;
+
+    GameObject backOuterRing;
+    GameObject backInnerRing;
+
+
+    // Use this for initialization
+    void Start () {
+        backOuterRing = transform.Find("Back Ring").Find("Outer Plate Ring").gameObject;
+        backInnerRing = transform.Find("Back Ring").Find("Inner Plate Ring").gameObject;
+    }
+
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetButton("Fire1"))
+        {
+            backOuterRing.GetComponent<MakePlateRings>().rotationSpeed = 3;
+            backInnerRing.GetComponent<MakePlateRings>().rotationSpeed = 3;
+            Instantiate(bullet);
+        }
+        else
+        {
+            backOuterRing.GetComponent<MakePlateRings>().rotationSpeed = 1;
+            backInnerRing.GetComponent<MakePlateRings>().rotationSpeed = 1;
+        }
+	}
+}
