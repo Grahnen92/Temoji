96025c25781da78262e80af10f24aeb38a1791a8
MythicWrath <jinxiang91@Hotmail.com>
2016-10-25 12:07:03 +0800

    Ring character update

--- Assets/Scripts/UI/Scripts/RingController.cs
+++ Assets/Scripts/UI/Scripts/RingController.cs
@@ -28,5 +28,16 @@ public class RingController : MonoBehaviour {
             backOuterRing.GetComponent<MakePlateRings>().rotationSpeed = 1;
             backInnerRing.GetComponent<MakePlateRings>().rotationSpeed = 1;
         }
-	}
+
+        var x = Input.GetAxis("Horizontal") * Time.deltaTime * 150.0f;
+        var z = Input.GetAxis("Vertical") * Time.deltaTime * 3.0f;
+
+        transform.Rotate(0, x, 0);
+        transform.Translate(0, 0, z);
+    }
+
+    void fireCmd()
+    {
+        //Random.insideUnitCircle;
+    }
 }
